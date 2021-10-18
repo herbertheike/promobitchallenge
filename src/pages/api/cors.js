0a983b9e0dfb78c5f642ff93bb631940672cbcac
herberth eike <herbertheike.ti@gmail.com>
2021-10-18 13:17:28 -0300

    18/10

--- src/pages/api/cors.js
+++ src/pages/api/cors.js
@@ -4,9 +4,5 @@ import cors from "cors";
 const handler = nc()
   // use connect based middleware
   .use(cors())
-  .post(async (req, res) => {
-    const response = await fetch(remoteServerUrl, config);
-    res.json(response);
-  });
 
 export default handler;
